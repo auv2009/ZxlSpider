88f7f92125aac5bb521f833d640e0121d42e9155
auv2009 <zxlreg@163.com>
2014-12-29 16:48:59 +0800

    branch changes

---
+++ Ca411Spider/Common/XmlConst.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ZxlSpider.Ca411Spider.Common
+{
+    public class XmlConst
+    {
+        public const string PROJ_NAME = "ZxlSpider";
+    }
+}
