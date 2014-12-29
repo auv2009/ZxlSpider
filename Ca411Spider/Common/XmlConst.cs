b58b400d1e02b3ea20bc0cb0de827b6b645ba90d
auv2009 <zxlreg@163.com>
2014-12-29 19:03:29 +0800

    change on branch1

--- Ca411Spider/Common/XmlConst.cs
+++ Ca411Spider/Common/XmlConst.cs
@@ -7,7 +7,7 @@ namespace ZxlSpider.Ca411Spider.Common
 {
     public class XmlConst
     {
-        public const string BRANCH = "branch1";
+        public const string BRANCH_1 = "change on branch1";
         public const string SERVER_IP = "192.168.8.162";
         public const string GITHUB_IP = "192.168.8.163";
     }
