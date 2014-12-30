1df8f5531500c262b02d131781197db89dbb322d
auv2009 <zxlreg@163.com>
2014-12-30 19:36:58 +0800

    comment

--- Ca411Spider/Common/XmlConst.cs
+++ Ca411Spider/Common/XmlConst.cs
@@ -7,8 +7,8 @@ namespace ZxlSpider.Ca411Spider.Common
 {
     public class XmlConst
     {
-        public const string B1 = "change on b1";
-        public const string B2 = "change on b2";
+        //public const string B1 = "change on b1";
+        //public const string B2 = "change on b2";
         public const string DEVELOP = "change on develop";
         public const string MASTER = "change on master";
         public const string SERVER_IP = "192.168.8.162";
