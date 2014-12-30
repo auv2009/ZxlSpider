75f42b0a84d22400ff83ce80354cca9ab3761a8b
auv2009 <zxlreg@163.com>
2014-12-30 17:55:09 +0800

    dev2

--- Ca411Spider/Common/XmlConst.cs
+++ Ca411Spider/Common/XmlConst.cs
@@ -9,7 +9,7 @@ namespace ZxlSpider.Ca411Spider.Common
     {
         public const string B1 = "change on b1";
         public const string B2 = "change on b2";
-        public const string DEVELOP = "change on master";
+        public const string DEVELOP = "change on develop";
         public const string SERVER_IP = "192.168.8.162";
         public const string GITHUB_IP = "192.168.8.163";
     }
